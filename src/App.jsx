ccb18b940a0699863de7692675fc6fc077f77a34
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-10 02:34:44 +0530

    Updated App.jsx, renders Authentication page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Authentication from './pages/Authentication'
 
 export default function App() {
   return (
     <div>
-      <h1 className="font-bold text-4xl text-center my-12">Hello World!</h1>
+      <Authentication />
     </div>
   )
 }
