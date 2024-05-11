a7acc848f835ff1c9ffde6c75b52b9952970da34
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-11 13:02:13 +0530

    Improved UI using Flowbite react

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react'
+import React, {useRef} from 'react'
+import { Button, TextInput, Textarea } from 'flowbite-react'
 
-export default function ProfileForm({hidden, onSubmit}) {
+export default function EditProfileForm({ onSubmit, hidden }) {
    const firstnameField = useRef(null)
    const lastnameField = useRef(null)
    const bioField = useRef(null)
@@ -9,29 +10,72 @@ export default function ProfileForm({hidden, onSubmit}) {
 
    function handleFormSubmit(e) {
       e.preventDefault()
-
       const formdata = new FormData()
 
-      formdata.append('first_name', firstnameField)
-      formdata.append('last_name', lastnameField)
-      formdata.append('bio', bioField)
-      formdata.append('country', countryField)
-      formdata.append('city', cityField)
+      formdata.append('first_name', firstnameField.current.value)
+      formdata.append('last_name', lastnameField.current.value)
+      formdata.append('bio', bioField.current.value)
+      formdata.append('country', countryField.current.value)
+      formdata.append('city', cityField.current.value)
 
       onSubmit(formdata)
    }
-
    return (
       <>
-         {!hidden && (
-            <form onSubmit={handleFormSubmit}>
-               <input ref={firstnameField} type="text" name='first_name' placeholder='firstname' />
-               <input ref={lastnameField} type="text" name='last_name' placeholder='lastname' />
-               <input ref={bioField} type="text" name='bio' placeholder='bio' />
-               <input ref={countryField} type="text" name='country' placeholder='country' />
-               <input ref={cityField} type="text" name='city' placeholder='city' />
-            </form>
-         )}
+         {!hidden && <form onSubmit={handleFormSubmit} className='flex flex-col items-center gap-2 w-full sm:max-w-md sm:mx-auto sm:shadow-xl sm:rounded-lg p-4 sm:border'>
+            <div className='text-center mb-8'>
+               <h1 className='font-bold text-cyan-700 text-4xl'>Create Profile</h1>
+               <h1 className='text-slate-600'>Enter your credentials to continue</h1>
+            </div>
+            <div className='grid gap-2 max-w-sm w-full'>
+               <TextInput
+                  type="text"
+                  name='first_name'
+                  id='first_name'
+                  placeholder='First name'
+                  ref={firstnameField}
+                  required
+               />
+               <TextInput
+                  type="text"
+                  name='last_name'
+                  id='last_name'
+                  placeholder='Last name'
+                  ref={lastnameField}
+                  required
+               />
+
+            </div>
+            <div className='grid gap-2 max-w-sm w-full'>
+               <Textarea
+                  name='bio'
+                  id='bio'
+                  placeholder='Describe yourself...'
+                  rows={4}
+                  ref={bioField}
+                  required
+               />
+            </div>
+            <div className='grid gap-2 max-w-sm w-full'>
+               <TextInput
+                  type='text'
+                  name='country'
+                  id='country'
+                  placeholder='Country'
+                  ref={countryField}
+                  required
+               />
+               <TextInput
+                  type='text'
+                  name='city'
+                  id='city'
+                  placeholder='City'
+                  ref={cityField}
+                  required
+               />
+            </div>
+            <Button className='max-w-sm w-full' type='submit'>Save</Button>
+         </form>}
       </>
    )
 }
