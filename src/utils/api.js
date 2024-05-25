0381655407318a999d5454fb894c038eca79a48a
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-25 19:44:54 +0530

    Improved authentication check

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,29 @@
 import axios from "axios"
-// import {accessToken, validateToken} from './token'
+import { getAccessToken } from './token'
+import { isUserAuthenticated } from "./auth"
 
 export const client = axios.create(
-    {
-        baseURL: 'http://127.0.0.1:8000',
-        withCredentials: true
-    }
+  {
+    baseURL: 'http://127.0.0.1:8000',
+    withCredentials: true,
+  }
 )
 
 client.interceptors.request.use(
-    (config) => {
-        // validateToken()
-        // config.headers.Authorization = `Bearer ${accessToken}`
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const accessToken = getAccessToken()
+
+    console.log(accessToken)
+    
+    if (isUserAuthenticated()) {
+      config.headers.Authorization = `Bearer ${accessToken}`
     }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
 )
 
 client.interceptors.response.use(
