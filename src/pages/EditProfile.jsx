51b8917ed17e8d0610285bbd0ba2b6cc1cf56a6f
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-25 19:42:10 +0530

    Created Edit Profile page

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProfileForm from '../components/ProfileForm'
+import { client } from '../utils/api'
+import { useNavigate } from 'react-router-dom'
+import { updateUserProfile } from '../utils/auth'
 
 export default function EditProfile() {
-  return (
-    <div>
-      
-    </div>
-  )
+   const [formErrors, setFormErrors] = useState(null)
+   const navigate = useNavigate()
+   function handleProfileEdit(fd) {
+      const base64ImageData = fd.get('profile_picture')
+      const binaryString = atob(base64ImageData.split(',')[1])
+      const byteArray = new Uint8Array(binaryString.length);
+      for (let i = 0; i < binaryString.length; i++) {
+         byteArray[i] = binaryString.charCodeAt(i);
+      }
+      const imageBlob = new Blob([byteArray], {type: 'image/webp'})
+
+      fd.delete('profile_picture')
+      fd.append('profile_picture', imageBlob, 'image.png')
+      updateUserProfile(fd).then((response) => {
+         if(response.type === 'success') {
+            navigate('/')
+         }
+      })
+   }
+   return (
+      <section className='h-screen grid place-items-center'>
+         <ProfileForm type='edit' messages={formErrors} onSubmit={(fd) => handleProfileEdit(fd)} />
+      </section>
+   )
 }
