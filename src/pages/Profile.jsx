89cea6af330e16b1036bfa0241d604af5148f9d7
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-25 19:43:57 +0530

    Created Profile Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,1 +1,38 @@
-rfc
+import React, { useEffect, useState } from 'react'
+import ProfileCard from '../components/ProfileCard'
+import { getUserProfile, isUserAuthenticated } from '../utils/auth'
+import Loading from '../components/Loading'
+import { useNavigate } from 'react-router-dom'
+
+export default function Profile() {
+   const [isLoading, setIsLoading] = useState(true)
+   const [userProfile, setUserProfile] = useState(null)
+   const navigate = useNavigate()
+
+   useEffect(() => {
+
+      console.log('Profile page in, authentication status is:', isUserAuthenticated())
+
+      getUserProfile().then((response) => {
+         if (response.type === 'success') {
+            setIsLoading(false)
+            setUserProfile(response.profile)
+            console.log(response.profile)
+         } else {
+            setIsLoading(false)
+            navigate('/create-profile')
+         }
+      }, [])
+
+   }, [])
+
+   return (
+      <>
+         {!isLoading ? (<section className='centered-grid'>
+            {userProfile && (
+               <ProfileCard profile={userProfile} />
+            )}
+         </section>) : (<Loading />)}
+      </>
+   )
+}
